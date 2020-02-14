afd9db8f06ec102123e4f73693e8573be382d2b6
diko <nbdinara@gmail.com>
2020-02-14 00:22:05 -0800

    WeekDay icons populates on TaskActivity depending on the schedule and current date

--- app/src/main/java/com/example/lyubishchevtiming/TaskActivity.java
+++ app/src/main/java/com/example/lyubishchevtiming/TaskActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.room.ColumnInfo;
 
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -22,6 +24,9 @@ import com.example.lyubishchevtiming.viewmodel.TaskViewModelFactory;
 import com.example.lyubishchevtiming.viewmodel.TaskWeekViewModel;
 import com.example.lyubishchevtiming.viewmodel.TaskWeekViewModelFactory;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import static android.content.ContentValues.TAG;
 
@@ -36,6 +41,13 @@ public class TaskActivity extends AppCompatActivity {
     private FloatingActionButton mAddEditTaskButton;
     private Button mStartButton;
     private AppDatabase mDb;
+    private ImageView mMonImageView;
+    private ImageView mTueImageView;
+    private ImageView mWedImageView;
+    private ImageView mThuImageView;
+    private ImageView mFriImageView;
+    private ImageView mSatImageView;
+    private ImageView mSunImageView;
 
 
     @Override
@@ -49,7 +61,8 @@ public class TaskActivity extends AppCompatActivity {
         if (intentThatStartedThisActivity != null) {
             if (intentThatStartedThisActivity.hasExtra("task")) {
                 mTask = intentThatStartedThisActivity.getExtras().getParcelable("task");
-                loadWeekFromDatabase(mTask.getWeekId());
+                Log.d(TAG, "onCreate: " + mTask.getWeekId());
+                loadWeekFromDatabase(String.valueOf(mTask.getWeekId()));
                 loadTaskFromDatabase(mTask.getId());
             }
         }
@@ -60,6 +73,13 @@ public class TaskActivity extends AppCompatActivity {
         mAddEditTaskButton = findViewById(R.id.fab_edit_add_task);
         mStartButton = findViewById(R.id.start_button);
 
+        mMonImageView = findViewById(R.id.mon_icon);
+        mTueImageView = findViewById(R.id.tue_icon);
+        mWedImageView = findViewById(R.id.wed_icon);
+        mThuImageView = findViewById(R.id.thu_icon);
+        mFriImageView = findViewById(R.id.fri_icon);
+        mSatImageView = findViewById(R.id.sat_icon);
+        mSunImageView = findViewById(R.id.sun_icon);
 
         mAddEditTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,7 +101,7 @@ public class TaskActivity extends AppCompatActivity {
             }
         });
 
-        setImageViewColor();
+
 
 
 
@@ -89,9 +109,78 @@ public class TaskActivity extends AppCompatActivity {
 
     public void populateUI(){
         mTaskNameTextView.setText(mTask.getName());
-
+        setImageViewColor();
+        setWeekDayIcons();
     }
 
+
+    public void setWeekDayIcons(){
+
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+
+
+        if (mWeek.getMon() == 0){
+            mMonImageView.setImageResource(R.mipmap.mon_disabled);
+        } else if (mWeek.getMon() !=0 && day == Calendar.MONDAY){
+            mMonImageView.setImageResource(R.mipmap.mon_today);
+        } else if (mWeek.getMon()!=0){
+            mMonImageView.setImageResource(R.mipmap.mon_enabled);
+        }
+
+        if (mWeek.getTue() == 0){
+            mTueImageView.setImageResource(R.mipmap.tue_disabled);
+        } else if (mWeek.getTue() !=0 && day == Calendar.TUESDAY){
+            mTueImageView.setImageResource(R.mipmap.tue_today);
+        } else if (mWeek.getTue()!=0){
+            mTueImageView.setImageResource(R.mipmap.tue_enabled);
+        }
+
+        if (mWeek.getWed() == 0){
+            mWedImageView.setImageResource(R.mipmap.wed_disabled);
+        } else if (mWeek.getWed() !=0 && day == Calendar.WEDNESDAY){
+            mWedImageView.setImageResource(R.mipmap.wed_today);
+        } else if (mWeek.getWed()!=0){
+            mWedImageView.setImageResource(R.mipmap.wed_enabled);
+        }
+
+        if (mWeek.getThu() == 0){
+            mThuImageView.setImageResource(R.mipmap.thu_disabled);
+        } else if (mWeek.getThu() !=0 && day == Calendar.THURSDAY){
+            mThuImageView.setImageResource(R.mipmap.thu_today);
+        } else if (mWeek.getThu()!=0){
+            mThuImageView.setImageResource(R.mipmap.thu_enabled);
+        }
+
+        if (mWeek.getFri() == 0){
+            mFriImageView.setImageResource(R.mipmap.fri_disabled);
+        } else if (mWeek.getFri() !=0 && day == Calendar.FRIDAY){
+            mFriImageView.setImageResource(R.mipmap.fri_today);
+          /*
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(150, 150);
+            mFriImageView.setLayoutParams(layoutParams);*/
+        } else if (mWeek.getFri()!=0){
+            mFriImageView.setImageResource(R.mipmap.fri_enabled);
+        }
+
+        if (mWeek.getSat() == 0){
+            mSatImageView.setImageResource(R.mipmap.sat_disabled);
+        } else if (mWeek.getSat() !=0 && day == Calendar.SATURDAY){
+            mSatImageView.setImageResource(R.mipmap.sat_today);
+        } else if (mWeek.getSat()!=0){
+            mSatImageView.setImageResource(R.mipmap.sat_enabled);
+        }
+
+        if (mWeek.getSun() == 0){
+            mSunImageView.setImageResource(R.mipmap.sun_disabled);
+        } else if (mWeek.getSun() !=0 && day == Calendar.SUNDAY){
+            mSunImageView.setImageResource(R.mipmap.sun_today);
+        } else if (mWeek.getSun()!=0){
+            mSunImageView.setImageResource(R.mipmap.sun_enabled);
+        }
+
+
+    }
 
     private void loadTaskFromDatabase(int id) {
         TaskViewModelFactory factory = new TaskViewModelFactory(mDb, id);
@@ -128,8 +217,10 @@ public class TaskActivity extends AppCompatActivity {
         });
     }
 
+
+
     private void loadWeekFromDatabase(String id) {
-        TaskWeekViewModelFactory factory = new TaskWeekViewModelFactory(mDb, id);
+        TaskWeekViewModelFactory factory = new TaskWeekViewModelFactory(mDb, String.valueOf(id));
         // COMPLETED (11) Declare a AddTaskViewModel variable and initialize it by calling ViewModelProviders.of
         // for that use the factory created above AddTaskViewModel
         final TaskWeekViewModel viewModel
